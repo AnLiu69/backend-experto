c6ad07292a60ef9e25dc5ef0cbbb8f2a9dde15d2
Rodrigo <rodrixfloresm@gmail.com>
2025-07-28 14:55:55 -0500

    Agregando el puerto en la conexion y en el .env

--- db/conexion.js
+++ db/conexion.js
@@ -4,7 +4,8 @@ const conexion = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
 });
 
 conexion.connect((err) => {
